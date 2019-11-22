7fb48663cb37eb645ca99802e4626d04e8c4f20e
valentinLampriere <valentin.lampriere1@gmail.com>
2019-11-22 20:45:22 +0100

    Lecture Csv
    début d'un lecture de csv

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		BufferedReader csvReader;
+		String row;
+		try {
+			csvReader = new BufferedReader(new FileReader("./Data/climat.csv"));
+			while ((row = csvReader.readLine()) != null) {
+			    String[] data = row.split("\t");
+			    System.out.println(data.length + " - " + data[1]);
+			}
+			csvReader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
