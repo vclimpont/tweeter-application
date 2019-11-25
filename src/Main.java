d3b097b84048c3c48aed2c671cb0b0200fce3eff
valentinLampriere <valentin.lampriere1@gmail.com>
2019-11-25 17:07:04 +0100

    Bouton import csv
    Interface "vite fait" pour selectionner un fichier csv, et lire les tweet

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,67 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+	private Stage primaryStage;
+    private BorderPane rootLayout;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Importation Donnees");
+        
+        initRootLayout();
+        
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("ImportationDonneesView.fxml"));
+            AnchorPane importationDonnee = (AnchorPane) loader.load();
+            
+            rootLayout.setCenter(importationDonnee);
+            
+            ImportationDonneesController controller = loader.getController();
+            controller.setMain(this);
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+	
+	public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+            
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 	public static void main(String[] args) {
-
+		launch(args);
+	}
+	
+	public void readTweets(String filename) {
 		BufferedReader csvReader;
 		String row;
 		int i = 1;
 		try {
-			csvReader = new BufferedReader(new FileReader("./Data/Foot.csv"));
+			csvReader = new BufferedReader(new FileReader("./Data/"+filename));
 			while ((row = csvReader.readLine()) != null) {
 			    String[] data = row.split("\t");
 			    System.out.println(i + " - " + data[1]);
@@ -20,7 +72,5 @@ public class Main {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 	}
-
 }
