c667894f57c2d4c973f55f42c0717cac3d7338ef
vclimpont <46527505+vclimpont@users.noreply.github.com>
2019-11-27 11:49:00 +0100

    css references

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Graph graph = new SingleGraph("Tutorial1");
-		graph.addAttribute("ui.stylesheet", "url('file:///C://Users//valen//eclipse-workspace//graphstream//stylesheets//stylesheet.css')");
+		graph.addAttribute("ui.stylesheet", "url('file:///..//tweeter-application//GraphStyle//stylesheet.css')");
 		
 		for(int i = 0; i < 30; i++)
 		{
